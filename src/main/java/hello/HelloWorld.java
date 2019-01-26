fd7df4592a6cab342d02b7b8c045646c386a70f7
jvenkateshmca <jalla.venkatesh.mca@gmail.com>
2019-01-26 08:21:00 +0530

    new maven files

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,15 +1,12 @@
 package hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
-     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-        System.out.println("Test");
-        System.out.println("Test for branch 1");
-        System.out.println("Test 123 for branch 1123");
-    }
-	public function view()
-	{
-		 System.out.println("Test123");
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
 	}
 }
