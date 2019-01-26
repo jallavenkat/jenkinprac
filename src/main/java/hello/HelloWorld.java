8651680e89d8584506920fd1751ddcf103f0a02c
jvenkateshmca <jalla.venkatesh.mca@gmail.com>
2019-01-26 08:15:43 +0530

    java files added

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class HelloWorld {
+     public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+        System.out.println("Test");
+        System.out.println("Test for branch 1");
+        System.out.println("Test 123 for branch 1123");
+    }
+	public function view()
+	{
+		 System.out.println("Test123");
+	}
+}
